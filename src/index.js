d8f6fdfc906a0b68b2882af3bc63beff98d25998
Gokce Yucel <gkcycl@gmail.com>
2017-05-21 16:12:37 +0300

    Rebased from master, added dragend event listened, getting new markers after dragenddd

--- src/index.js
+++ src/index.js
@@ -1,66 +1,9 @@
-<<<<<<< HEAD
-import http from 'http';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import initializeDb from './db';
-import middleware from './middleware';
-import api from './api';
-import publicApp from './public';
-import config from './config';
-import ejs from 'ejs';
-import path from 'path';
-=======
 import app from './server';
 import config from './config.json';
->>>>>>> Test implementations using mocha
 
 const port = process.env.PORT || config.port;
 
-<<<<<<< HEAD
-// view engine setup
-app.set('views', path.join(__dirname, 'public/views'));
-app.set('view engine', 'ejs');
-app.engine('html', ejs.renderFile);
-
-// set static files directory
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-app.server = http.createServer(app);
-
-// logger
-app.use(morgan('dev'));
-
-// 3rd party middleware
-app.use(cors({
-	exposedHeaders: config.corsHeaders
-}));
-
-app.use(bodyParser.json({
-	limit: config.bodyLimit
-}));
-
-// connect to db
-initializeDb(db => {
-
-	// internal middleware
-	app.use(middleware({ config, db }));
-
-	// api router
-	app.use('/api', api({ config, db }));
-
-	// app router
-	app.use('/app', publicApp({ config, db }));
-
-	const port = process.env.PORT || config.port;
-
-	app.server.listen(port);
-
-	console.log(`Started on port ${port}`);
-=======
 app.server.listen(port, (err) => {
-  if (err) return console.error(err);
-  console.log(`Server listening port ${port}`);
->>>>>>> Test implementations using mocha
+	if (err) return console.error(err);
+	console.log(`Server listening port ${port}`);
 });
